3b4525746cdac64c09592a3fe8bafd38be004b78
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-22 20:59:22 -0700

    Grid Layout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,40 @@ function App() {
     <div className="App">
       <div className="title">Hik Hak Joe</div>
       <div className="box-container">
-        <div className="game-box">in the game</div>
+        <div className="game-box">
+          <div className="row-container">
+            <div className="row">
+              <div className="row-item"> row item 1</div>
+              <div className="row-item"> row item 2</div>
+              <div className="row-item"> row item 3</div>
+              <div className="row-item"> row item 4</div>
+            </div>
+          </div>
+          <div className="row-container">
+            <div className="row">
+              <div className="row-item"> row item 1</div>
+              <div className="row-item"> row item 2</div>
+              <div className="row-item"> row item 3</div>
+              <div className="row-item"> row item 4</div>
+            </div>
+          </div>
+          <div className="row-container">
+            <div className="row">
+              <div className="row-item"> row item 1</div>
+              <div className="row-item"> row item 2</div>
+              <div className="row-item"> row item 3</div>
+              <div className="row-item"> row item 4</div>
+            </div>
+          </div>
+          <div className="row-container">
+            <div className="row">
+              <div className="row-item"> row item 1</div>
+              <div className="row-item"> row item 2</div>
+              <div className="row-item"> row item 3</div>
+              <div className="row-item"> row item 4</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
