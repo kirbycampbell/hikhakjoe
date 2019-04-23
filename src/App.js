5a36c529e345294eec93df43c2cfe599b180fa3f
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-23 12:29:11 -0700

    Adding reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { StoreContext, StoreProvider } from "./context/StoreContext";
 
 import "./App.css";
 import BoxGrid from "./Components/BoxGrid";
 
 function App() {
+  const { state, dispatch, actions } = useContext(StoreContext);
+
   return (
     <div className="App">
       <div className="title">Hik Hak Joe</div>
---
+++ src/context/StoreContext.js
@@ -0,0 +1,20 @@
+import React, { useEffect, createContext, useReducer } from "react";
+import { initialState, types, reducer } from "./reducers";
+import { useActions } from "./action";
+
+const StoreContext = createContext(initialState);
+const StoreProvider = ({ children }) => {
+  // Get state and dispatch from Reacts new API useReducer.
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // Get actions from useActions and pass it to Context
+  const actions = useActions(state, dispatch);
+  // Log new state
+  useEffect(() => console.log({ newState: state }), [state]);
+  // Render state, dispatch and special case actions
+  return (
+    <StoreContext.Provider value={{ state, dispatch, actions }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+export { StoreContext, StoreProvider };
