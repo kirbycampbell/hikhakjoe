74c0401ede8d35c28ec31947f7d507c421c29684
kirbycampbell <jkirbycampbell@gmail.com>
2019-04-25 17:05:48 -0700

    Seperated InnerBoxes to own component

--- src/Components/BoxGrid.js
+++ src/Components/BoxGrid.js
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import { StoreContext } from "../context/StoreContext";
 import "../CSS/BoxGrid.css";
 import { types } from "../context/reducers";
+import InnerBoxes from "./InnerBoxes";
 
 const winningCombos = [
   [0, 1, 2],
@@ -121,141 +122,7 @@ const BoxGrid = () => {
   return (
     <div className="box-container">
       {!state.winner && !state.endGame && (
-        <div className="game-box">
-          <div className="row-container">
-            <div className="row">
-              <div
-                className="row-item row-itemL"
-                onClick={() => handleBoxClick(0)}
-              >
-                {board[0] === "x" && <i className="fas fa-times game-piece" />}
-                {board[0] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(1)}>
-                {board[1] === "x" && <i className="fas fa-times game-piece" />}
-                {board[1] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(2)}>
-                {board[2] === "x" && <i className="fas fa-times game-piece" />}
-                {board[2] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(3)}>
-                {board[3] === "x" && <i className="fas fa-times game-piece" />}
-                {board[3] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="row-container">
-            <div className="row">
-              <div
-                className="row-item row-itemL"
-                onClick={() => handleBoxClick(4)}
-              >
-                {board[4] === "x" && <i className="fas fa-times game-piece" />}
-                {board[4] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(5)}>
-                {board[5] === "x" && <i className="fas fa-times game-piece" />}
-                {board[5] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(6)}>
-                {board[6] === "x" && <i className="fas fa-times game-piece" />}
-                {board[6] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(7)}>
-                {board[7] === "x" && <i className="fas fa-times game-piece" />}
-                {board[7] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="row-container">
-            <div className="row">
-              <div
-                className="row-item row-itemL"
-                onClick={() => handleBoxClick(8)}
-              >
-                {board[8] === "x" && <i className="fas fa-times game-piece" />}
-                {board[8] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(9)}>
-                {board[9] === "x" && <i className="fas fa-times game-piece" />}
-                {board[9] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(10)}>
-                {board[10] === "x" && <i className="fas fa-times game-piece" />}
-                {board[10] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div className="row-item" onClick={() => handleBoxClick(11)}>
-                {board[11] === "x" && <i className="fas fa-times game-piece" />}
-                {board[11] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="row-container">
-            <div className="row">
-              <div
-                className="row-item row-itemL row-itemB"
-                onClick={() => handleBoxClick(12)}
-              >
-                {board[12] === "x" && <i className="fas fa-times game-piece" />}
-                {board[12] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div
-                className="row-item row-itemB"
-                onClick={() => handleBoxClick(13)}
-              >
-                {board[13] === "x" && <i className="fas fa-times game-piece" />}
-                {board[13] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div
-                className="row-item row-itemB"
-                onClick={() => handleBoxClick(14)}
-              >
-                {board[14] === "x" && <i className="fas fa-times game-piece" />}
-                {board[14] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-              <div
-                className="row-item row-itemB"
-                onClick={() => handleBoxClick(15)}
-              >
-                {board[15] === "x" && <i className="fas fa-times game-piece" />}
-                {board[15] === "o" && (
-                  <i className="far fa-dot-circle game-piece" />
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
+        <InnerBoxes handleBoxClick={handleBoxClick} board={board} />
       )}{" "}
       {state.winner && (
         <React.Fragment>
---
+++ src/Components/InnerBoxes.js
@@ -0,0 +1,179 @@
+import React, { useState, useContext, useEffect } from "react";
+import { StoreContext } from "../context/StoreContext";
+import "../CSS/BoxGrid.css";
+import "../CSS/InnerBoxes.css";
+import { types } from "../context/reducers";
+
+const InnerBoxes = props => {
+  return (
+    <div className="game-box">
+      <div className="row-container">
+        <div className="row">
+          <div
+            className="row-item row-itemL"
+            onClick={() => props.handleBoxClick(0)}
+          >
+            {props.board[0] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[0] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(1)}>
+            {props.board[1] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[1] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(2)}>
+            {props.board[2] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[2] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(3)}>
+            {props.board[3] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[3] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="row-container">
+        <div className="row">
+          <div
+            className="row-item row-itemL"
+            onClick={() => props.handleBoxClick(4)}
+          >
+            {props.board[4] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[4] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(5)}>
+            {props.board[5] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[5] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(6)}>
+            {props.board[6] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[6] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(7)}>
+            {props.board[7] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[7] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="row-container">
+        <div className="row">
+          <div
+            className="row-item row-itemL"
+            onClick={() => props.handleBoxClick(8)}
+          >
+            {props.board[8] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[8] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(9)}>
+            {props.board[9] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[9] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(10)}>
+            {props.board[10] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[10] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div className="row-item" onClick={() => props.handleBoxClick(11)}>
+            {props.board[11] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[11] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="row-container">
+        <div className="row">
+          <div
+            className="row-item row-itemL row-itemB"
+            onClick={() => props.handleBoxClick(12)}
+          >
+            {props.board[12] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[12] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div
+            className="row-item row-itemB"
+            onClick={() => props.handleBoxClick(13)}
+          >
+            {props.board[13] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[13] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div
+            className="row-item row-itemB"
+            onClick={() => props.handleBoxClick(14)}
+          >
+            {props.board[14] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[14] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+          <div
+            className="row-item row-itemB"
+            onClick={() => props.handleBoxClick(15)}
+          >
+            {props.board[15] === "x" && (
+              <i className="fas fa-times game-piece" />
+            )}
+            {props.board[15] === "o" && (
+              <i className="far fa-dot-circle game-piece" />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InnerBoxes;
