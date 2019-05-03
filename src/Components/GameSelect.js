d5c3209c07fefc4dbdca0da1ce024d0a0abdfd03
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-02 22:07:49 -0700

    Add game select title

--- src/Components/GameSelect.js
+++ src/Components/GameSelect.js
@@ -11,16 +11,19 @@ const GameSelect = () => {
     dispatch({ type: types.GAME_TYPE, payload: { type_of_game: selection } });
   };
   return (
-    <div className="game-select-container">
-      <button onClick={() => handleGameType("zero")} className="sel-btn">
-        <div className="btn-text">0 Player Game</div>
-      </button>
-      <button onClick={() => handleGameType("single")} className="sel-btn">
-        <div className="btn-text">1 Player Game</div>
-      </button>
-      <button onClick={() => handleGameType("duel")} className="sel-btn">
-        <div className="btn-text">2 Player Game</div>
-      </button>
+    <div className="main-select-container">
+      <div className="title-select">Select Type of Game: </div>
+      <div className="game-select-container">
+        <button onClick={() => handleGameType("zero")} className="sel-btn">
+          <div className="btn-text">0 Player Game</div>
+        </button>
+        <button onClick={() => handleGameType("single")} className="sel-btn">
+          <div className="btn-text">1 Player Game</div>
+        </button>
+        <button onClick={() => handleGameType("duel")} className="sel-btn">
+          <div className="btn-text">2 Player Game</div>
+        </button>
+      </div>
     </div>
   );
 };
