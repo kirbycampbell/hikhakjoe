12f9fce3a5a57cf9a7dbab1acef6d9e5ab27dc18
kirbycampbell <jkirbycampbell@gmail.com>
2019-05-03 12:39:48 -0700

    CSS Transition Work

--- src/Components/GamePoints.js
+++ src/Components/GamePoints.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { StoreContext } from "../context/StoreContext";
 import useInterval from "./useInterval";
+import { CSSTransition } from "react-transition-group";
 import "../CSS/GamePoints.css";
 
 const GamePoints = () => {
@@ -19,33 +20,51 @@ const GamePoints = () => {
   useEffect(() => {
     setPointsChangeP1(false);
     setPointsChangeP2(false);
-  }, []);
+  }, [pointsChangeP1, pointsChangeP2]);
 
-  useInterval(
-    () => {
-      if (pointsChangeP1) {
-        setPointsChangeP1(false);
-      } else if (pointsChangeP2) {
-        setPointsChangeP2(false);
-      }
-    },
-    [pointsChangeP1 ? 2000 : null],
-    [pointsChangeP2 ? 2000 : null]
-  );
+  // useInterval(
+  //   () => {
+  //     if (pointsChangeP1) {
+  //       setPointsChangeP1(false);
+  //     } else if (pointsChangeP2) {
+  //       setPointsChangeP2(false);
+  //     }
+  //   },
+  //   [pointsChangeP1 ? 2000 : null],
+  //   [pointsChangeP2 ? 2000 : null]
+  // );
 
   return (
     <div className="upper-container">
       <div className="game-point">Game Points:</div>
-      {!pointsChangeP1 && (
-        <div className="points">
-          P1 Points: <div className="num">{state.p1Points}</div>
-        </div>
-      )}
-      {pointsChangeP1 && (
+      <CSSTransition
+        in={pointsChangeP1}
+        timeout={2000}
+        classNames="showScore"
+        unmountOnExit
+        onEnter={() => setPointsChangeP1(true)}
+        onExited={() => setPointsChangeP1(false)}
+      >
         <div className="points big">
           P1 Scores! <div className="num big">{state.p1Points}</div>
         </div>
-      )}
+      </CSSTransition>
+      <CSSTransition
+        in={!pointsChangeP1}
+        timeout={2000}
+        classNames="showPoints"
+        unmountOnExit
+      >
+        <div className="points">
+          P1 Points: <div className="num">{state.p1Points}</div>
+        </div>
+      </CSSTransition>
+
+      {/* {!pointsChangeP1 && (
+        <div className="points big">
+          P1 Score: <div className="num">{state.p1Points}</div>
+        </div>
+      )} */}
       {!pointsChangeP2 && (
         <div className="points">
           P2 Points: <div className="num">{state.p2Points}</div>
